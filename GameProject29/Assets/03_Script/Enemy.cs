953474326c3ba7d1ee370f496e03367958bcda8e
Dazs <nkxpz3@gmail.com>
2021-10-21 13:15:22 +0900

    enemy 体力設定

--- GameProject29/Assets/03_Script/Enemy.cs
+++ GameProject29/Assets/03_Script/Enemy.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : Facade {
-    
+
+    // Hide  Property ==========================================================================================
+    private float   _defHp;
+    private int     _defLife;
+
     // Show  Property =============================================
     [SerializeField][Range(0.0f, 1.0f)] private float   _hp;
     [SerializeField]                    private int     _life;
     
     // User  Method ===============================================
     private void Awake  () {
-        GetSetPosition = transform.position;   
+        GetSetPosition = transform.position;
+        _defHp      = _hp;
+        _defLife    = _life;
     }
-    
+
+    private void OnDisable() {
+        _hp         = _defHp;
+        _life       = _defLife;
+    }
+
     // User  Method ===============================================
     public  float        GetSetHp        {
         get { return _hp;  }
