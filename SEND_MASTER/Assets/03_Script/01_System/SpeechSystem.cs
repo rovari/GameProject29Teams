db9ce6a87e1bcec839634df8965f70664a914ace
Dazs <nkxpz3@gmail.com>
2021-12-16 20:32:15 +0900

    speechsystem

--- SEND_MASTER/Assets/03_Script/01_System/SpeechSystem.cs
+++ SEND_MASTER/Assets/03_Script/01_System/SpeechSystem.cs
@@ -9,7 +9,6 @@ public enum SPEECH_BUBBLE {
     NORMAL,
     THORN,
     THINK,
-    TUTORIAL
 }
 
 [Serializable]
@@ -17,8 +16,7 @@ public struct Speech {
 
     public SPEECH_BUBBLE    bubble;
     public string           speech;
-    public Sprite           icon;
-    public bool             isUseButton;
+    public bool             isButton;
 }
 
 public class SpeechSystem : MonoBehaviour {
@@ -29,66 +27,70 @@ public class SpeechSystem : MonoBehaviour {
     [Space(10)]
     [SerializeField] private GameObject     targetCunvas;
     [SerializeField] private Image          bubbleImage;
-    [SerializeField] private Image          iconImage;
     [SerializeField] private Text           targetText;
 
     [Space(10)]
     [SerializeField] private Sprite         normalBubble;
     [SerializeField] private Sprite         thinkBubble;
     [SerializeField] private Sprite         thornBubble;
-    [SerializeField] private Sprite         tutorialBubble;
 
     private int     currentIndex;
     private int     oldIndex;
     private bool    isFinish;
+    private bool    isLock;
 
     // Property
     
     // Method
     public  void Speech     () {
+        
+        if (speecheList.Count < 0) {
 
-        if (speecheList.Count <= 0) {
+            isLock      = true;
+            isFinish    = true;
 
             targetCunvas.SetActive(false);
-            isFinish = true;
             return;
         }
         
         if(oldIndex != currentIndex) {
-            oldIndex = currentIndex;
 
-            switch (speecheList[currentIndex].bubble) {
+            isLock = true;
+
+            if (currentIndex < speecheList.Count) {
+
+                oldIndex = currentIndex;
+            
+                switch (speecheList[currentIndex].bubble) {
+                    
+                    case SPEECH_BUBBLE.NORMAL:
+                        bubbleImage.sprite = normalBubble;             
+                        break;
+
+                    case SPEECH_BUBBLE.THINK:
+                        bubbleImage.sprite = thinkBubble;
+                        break;
+
+                    case SPEECH_BUBBLE.THORN:
+                        bubbleImage.sprite = thornBubble;
+                        break;
+                        
+                    default: break;
+                }
+            }
+            else {
                 
-                case SPEECH_BUBBLE.NORMAL:
-                    bubbleImage.sprite = normalBubble;             
-                    break;
-
-                case SPEECH_BUBBLE.THINK:
-                    bubbleImage.sprite = thinkBubble;
-                    break;
-
-                case SPEECH_BUBBLE.THORN:
-                    bubbleImage.sprite = thornBubble;
-                    break;
-
-                case SPEECH_BUBBLE.TUTORIAL:
-                    bubbleImage.sprite = tutorialBubble;
-                    break;
-
-                default: break;
+                targetCunvas.SetActive(false);
+                isFinish = true;
+                return;
             }
 
             targetText.text     = speecheList[currentIndex].speech;
-            iconImage.sprite    = speecheList[currentIndex].icon;
-        }
-
-        if (speecheList.Count == currentIndex) {
-            targetCunvas.SetActive(false);
-            isFinish = true;
+            isLock              = false;
         }
     }
     private void AddIndex   () {
-        if (speecheList[currentIndex].isUseButton) ++currentIndex;
+        if (speecheList[currentIndex].isButton && !isLock) ++currentIndex;
     }
     public  bool GetIsFinish() {
         return isFinish;
@@ -104,7 +106,7 @@ public class SpeechSystem : MonoBehaviour {
 	private void Start() {
 
         oldIndex = -1;
-        //InputManager.GetEVENTActions();
+        InputManager.GetEVENTActions().Apply.started += _ => AddIndex();
 	}
     
 }
